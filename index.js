590bce0a0253d26da831c8f19542879a2bb95e75
nurulamin092 <mnurulamin092@gmial.com>
2022-12-07 14:01:51 +0600

    connet to mongodb atlas

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const cors = require('cors');
-require('dotenv').config(); 
+
 const port = process.env.PORT || 4000;
  
 const app = express ();  
@@ -13,31 +14,49 @@ const app = express ();
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://dbuser1:7KcRgVrvNuLNOHuL@cluster0.yoijtmg.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, 
+ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yoijtmg.mongodb.net/?retryWrites=true&w=majority`;
+
+    const client = new MongoClient(uri, 
     { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 
     });
-// client.connect(err => {
-//   const collection = client.db("moontech").collection("productxx");
-//   console.log('db connected');
-//   // perform actions on the collection object
-//   client.close();
-// });
+
+
 
 async function run () {
     try {
         await client.connect();
       
-        const productCollection =client.db("moontech").collection("user");
-        const user = {name:'Abdullah',email:'abdullah@gmail.com'}
-          const result = productCollection.insertOne(user);
-          console.log(`Product inserted with id: ${(await result).insertedId}`);
+        const productCollection =client.db("moontech").collection("product");
+        
+        app.get("/products", async (req, res) => {
+            const cursor = productCollection.find({});
+            const product = await cursor.toArray();
+    
+            res.send({ status: true, data: product });
+        });
+
+        app.post("/product", async (req, res) => {
+            const product = req.body;
+      
+            const result = await productCollection.insertOne(product);
+      
+            res.send(result);
+          });
+      
+          app.delete("/product/:id", async (req, res) => {
+            const id = req.params.id;
+      
+            const result = await productCollection.deleteOne({ _id: ObjectId(id) });
+            res.send(result);
+          });
+
 
     } finally {
         // await client.close()
     }
 }
-run().catch(console.dir)
+
+run().catch((err) => console.log(err));
 
 
 
